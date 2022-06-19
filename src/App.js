3c0a55a68a82f5d0b44f2c1f7da36c0e76ea6fad
Rafiul29 <rafiulislam4472@gmail.com>
2022-06-19 22:37:51 +0600

    name update

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import { useEffect, useState } from 'react';
 function App() {
 
   const[names, setNames]=useState([
-    
 ])
 const[name, setName]=useState('')
   const Save=async(e)=>{
-    
-    
     console.log("hello")
       await axios.post("http://localhost:5000/save",{
         name
@@ -19,9 +16,7 @@ const[name, setName]=useState('')
       .then((data)=>{
         console.log(data)
       })
-      
   }
-  
   useEffect(()=>{
     getName();
   },[])
@@ -31,35 +26,55 @@ const[name, setName]=useState('')
       setNames(data.data);
     })
 }
+//updateName.......
+const updateName=(id)=>{
+ axios.put(`http://localhost:5000/update/${id}`,{name})
+ console.log(id)
+ .then(res=>{
+  console.log(res.data)
+})
+.catch(err=>{
+   console.log(err)
+})
+}
+
+// delete name......
+const deletename=(id)=>{
+  axios.delete(`http://localhost:5000/delete/${id}`)
+}
 
   return (
    
-    <div style={{width: "50%"}}>
+    <div className='container my-5' style={{width: "80%"}}>
     <table className="table table-danger table-striped">
       <thead>
         <tr>
          <th scope="col">SL no.</th>
           <th scope="col"> Name</th>
+          <th scope="col"> D/E</th>
         </tr>
       </thead>
       <tbody>
       {names.length>0 &&
-    names.map((item,index)=>{
+       names.map((item,index)=>{
       return(
         <tr key={index}>
         <th scope="row">{index+1}</th>
        <td>{item.name}</td>
+       <td>
+       <input type="text" placeholder='Update Name...'  onChange={(e) => {
+              setName(e.target.value)
+            }}/>
+       <button className="btn btn-success" onClick={()=>{updateName(item._id)}}>Update</button>/
+        <button className='btn btn-danger' onClick={()=>{deletename(item._id)}}>Delete</button>
+       
+       </td>
         </tr>
       )
     })}
       </tbody>
-
-
-   
-
-  
     </table>
-    <input type="text" className="form-control"  placeholder='enter your name' onChange={(e)=>setName(e.target.value)}></input>
+  <input type="text" className="form-control"  placeholder='Enter your name' onChange={(e)=>setName(e.target.value)}></input>
    <button onClick={()=>Save()}  className="btn btn-primary" type="button">Create</button>
  
     </div>
